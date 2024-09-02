849cbc60608698917cffb88f38d118d2c5908774
dvdalfsen <70368741+dvdalfsen@users.noreply.github.com>
2024-09-02 12:33:16 +0200

    Aanpassing visitekaartje

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') // selecteerd de class header button 
+expandButton.addEventListener('click', expand) //als op de button word geclickt word de functie expand uitgevoerd en dan word het visite kaartje groter.
 
 function expand () {
-  document.body.classList.toggle('expand')
+  document.body.classList.toggle('expand') // funtie voor het groter maken van het visite kaartje.
 }
